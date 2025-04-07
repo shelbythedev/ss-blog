61fe1b1450c95b1c93091bc40bb32bc9e15eb027
Shelby Solomon <shelby@shelbysolomon.net>
2025-04-07 08:44:30 -0500

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+const { config } = require("dotenv");
+
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+    siteMetadata: {
+        title: `Shelby Solomon`,
+        siteUrl: process.env.SITE_URL,
+        description: `Shelby Solomon's personal website`
+    },
+    plugins: [{
+        resolve: 'gatsby-source-contentful',
+        options: {
+            "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
+            "spaceId": process.env.CONTENTFUL_SPACE_ID
+        }
+    }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+        resolve: "gatsby-plugin-postcss",
+        options: {
+            postCssPlugins: [
+                require('postcss-import'),
+                require('@tailwindcss/postcss')({config: './tailwind.config.js'}),
+                require('autoprefixer'),
+                require('postcss-preset-env')({
+                    stage: 1
+                }),
+            ],
+        },
+    }, "gatsby-plugin-sitemap", {
+        resolve: 'gatsby-plugin-manifest',
+        options: {
+            "icon": "src/images/icon.png"
+        }
+    }, "gatsby-plugin-mdx", {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            "name": "pages",
+            "path": "./src/pages/"
+        },
+        __key: "pages"
+    }]
+};
