0f2883ccc1ad39deb4fade2d89df136edfae9050
Shelby Solomon <shelby@shelbysolomon.net>
2025-04-08 16:57:35 -0500

    clean up; url to env var

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,13 +38,6 @@ module.exports = {
             options: {
                 "icon": "src/images/icon.png"
             }
-        // }, "gatsby-plugin-mdx", {
-        //     resolve: 'gatsby-source-filesystem',
-        //     options: {
-        //         "name": "pages",
-        //         "path": "./src/pages/"
-        //     },
-        //     __key: "pages"
         }
     ]
 };
